ff3ce9cd1187a1357008627d3913db6e7e4b2ee5
Zane Suhadolnik <zanesuhi@gmail.com>
2019-06-26 06:52:32 +0200

    [Add] Testing styling for dark mode

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -13,32 +13,7 @@ twinPeaksTheme.overrideThemeStyles = () => ({
   },
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
-  },
-  // These two are for gatsby-remark-autolink-headers:
-  'a.anchor': {
-    boxShadow: 'none',
-  },
-  'a.anchor svg[aria-hidden="true"]': {
-    stroke: 'var(--textLink)',
-  },
-  'p code': {
-    fontSize: '1rem',
-  },
-  // TODO: why tho
-  'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code': {
-    fontSize: 'inherit',
-  },
-  'li code': {
-    fontSize: '1rem',
-  },
-  blockquote: {
-    color: 'inherit',
-    borderLeftColor: 'inherit',
-    opacity: '0.8',
-  },
-  'blockquote.translation': {
-    fontSize: '1em',
-  },
+  }
 })
 
 delete twinPeaksTheme.googleFonts
